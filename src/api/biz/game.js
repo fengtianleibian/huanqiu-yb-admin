52f17c7dccb6bdcc7669f5397f27d835ff7dcc80
Eason <>
2025-09-22 10:29:19 +0700

    no message

--- src/api/biz/game.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 获取游戏列表
-export function getGameList() {
-  return request({
-    url: 'system/api/game/list',
-    method: 'get'
-  })
-}
--- src/api/biz/member_bet_log.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-// 获取用户下注日志列表
-export function getMemberBetLogPage(data) {
-  return request({
-    url: '/system/api/member_bet_log/page',  method: 'post', data: data
-  })
-}
