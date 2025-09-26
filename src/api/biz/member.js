2e20165d3bc2b1654f21c6b49be3eca47439b343
Eason <>
2025-09-26 11:33:33 +0700

    no message

--- src/api/biz/member.js
+++ src/api/biz/member.js
@@ -8,3 +8,20 @@ export function listMember(query) {
     data: query
   })
 }
+
+// 查询用户登录日志列表
+export function listMemberLoginLog(query) {
+  return request({
+    url: '/system/api/member_login_log/list',
+    method: 'post',
+    data: query
+  })
+}
+
+// 删除用户登录日志
+export function delMemberLoginLog(ids) {
+  return request({
+    url: '/system/api/member_login_log/' + ids,
+    method: 'post'
+  })
+}
--- src/views/biz/member/member_login_log/index.vue
+++ src/views/biz/member/member_login_log/index.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="IP地址" prop="ip">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="80px">
+      <el-form-item label="用户ID" prop="memberId">
         <el-input
-          v-model="queryParams.ip"
-          placeholder="请输入IP地址"
+          v-model="queryParams.memberId"
+          placeholder="请输入用户ID"
           clearable
           @keyup.enter.native="handleQuery"
         />
+      </el-form-item>
+      <el-form-item label="用户名" prop="memberName">
+        <el-input
+          v-model="queryParams.memberName"
+          placeholder="请输入用户名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择状态" clearable>
+          <el-option label="成功" value="0"/>
+          <el-option label="失败" value="1"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="登录时间">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -18,83 +43,49 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['ip_whitelist:add']"
-        >新增
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['ip_whitelist:edit']"
-        >修改
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="danger"
           plain
           icon="el-icon-delete"
           size="mini"
           :disabled="multiple"
           @click="handleDelete"
-          v-hasPermi="['ip_whitelist:remove']"
+          v-hasPermi="['member_login_log:remove']"
         >删除
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-refresh"
-          size="mini"
-          @click="handleRefreshCache"
-          v-hasPermi="['ip_whitelist:remove']"
-        >刷新缓存
         </el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="ipWhitelistList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="memberLoginLogList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center"/>
-      <el-table-column label="ID" align="center" prop="id"/>
-      <el-table-column label="IP地址" align="center" prop="ip"/>
-      <el-table-column label="创建时间" align="center" prop="createTime">
+      <el-table-column label="用户ID" align="center" prop="memberId"/>
+      <el-table-column label="用户名" align="center" prop="memberName"/>
+      <el-table-column label="登录IP" align="center" prop="loginIp"/>
+      <el-table-column label="登录地点" align="center" prop="loginLocation" show-overflow-tooltip/>
+      <el-table-column label="浏览器类型" align="center" prop="browser"/>
+      <el-table-column label="操作系统" align="center" prop="os"/>
+      <el-table-column label="设备类型" align="center" prop="deviceType"/>
+      <el-table-column label="域名" align="center" prop="domain" show-overflow-tooltip width="160"/>
+      <el-table-column label="状态" align="center" prop="status">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 0" type="success">成功</el-tag>
+          <el-tag v-else type="danger">失败</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="失败原因" align="center" prop="failRemark" show-overflow-tooltip/>
+      <el-table-column label="登录时间" align="center" prop="createTime" width="160">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="更新时间" align="center" prop="updateTime">
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="100" fixed="right">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.updateTime) }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['ip_whitelist:edit']"
-          >修改
-          </el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['ip_whitelist:remove']"
+            v-hasPermi="['member_login_log:remove']"
           >删除
           </el-button>
         </template>
@@ -108,68 +99,39 @@
       :limit.sync="queryParams.pageSize"
       @pagination="getList"
     />
-
-    <!-- 添加或修改IP白名单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="IP地址" prop="ip">
-          <el-input v-model="form.ip" placeholder="请输入IP地址"/>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import {
-  listIpWhitelist,
-  getIpWhitelist,
-  delIpWhitelist,
-  addIpWhitelist,
-  updateIpWhitelist,
-  refreshCache
-} from "@/api/system/ip_whitelist";
+import {listMemberLoginLog, delMemberLoginLog} from "@/api/biz/member";
 
 export default {
-  name: "IpWhitelist",
+  name: "MemberLoginLog",
   data() {
     return {
       // 遮罩层
       loading: true,
       // 选中数组
       ids: [],
-      // 非单个禁用
-      single: true,
       // 非多个禁用
       multiple: true,
       // 显示搜索条件
       showSearch: true,
       // 总条数
       total: 0,
-      // IP白名单表格数据
-      ipWhitelistList: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
+      // 用户登录日志表格数据
+      memberLoginLogList: [],
+      // 日期范围
+      dateRange: [],
       // 查询参数
       queryParams: {
         pageNum: 1,
         pageSize: 10,
-        ip: undefined
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        ip: [
-          {required: true, message: "IP地址不能为空", trigger: "blur"},
-          {pattern: /^(\d{1,3}\.){3}\d{1,3}$/, message: "请输入正确的IP地址格式", trigger: "blur"}
-        ]
+        memberId: undefined,
+        memberName: undefined,
+        status: undefined,
+        beginTime: undefined,
+        endTime: undefined
       }
     };
   },
@@ -177,31 +139,19 @@ export default {
     this.getList();
   },
   methods: {
-    /** 查询IP白名单列表 */
+    /** 查询用户登录日志列表 */
     getList() {
       this.loading = true;
-      listIpWhitelist(this.queryParams).then(response => {
+      this.addDateRange();
+      listMemberLoginLog(this.queryParams).then(response => {
         if (response.code === 200) {
-          this.ipWhitelistList = response.content.list;
+          this.memberLoginLogList = response.content.list;
           this.total = response.content.total;
           this.loading = false;
         } else {
           this.$modal.msgError(response.message);
         }
       });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        id: undefined,
-        ip: undefined
-      };
-      this.resetForm("form");
     },
     /** 搜索按钮操作 */
     handleQuery() {
@@ -210,68 +160,22 @@ export default {
     },
     /** 重置按钮操作 */
     resetQuery() {
+      this.dateRange = [];
+      this.queryParams.beginTime = undefined;
+      this.queryParams.endTime = undefined;
       this.resetForm("queryForm");
       this.handleQuery();
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length != 1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加IP白名单";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const id = row.id || this.ids
-      getIpWhitelist(id).then(response => {
-        if (response.code === 200) {
-          this.form = response.content;
-          this.open = true;
-          this.title = "修改IP白名单";
-        } else {
-          this.$modal.msgError(response.message);
-        }
-      });
-    },
-    /** 提交按钮 */
-    submitForm: function () {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.id != undefined) {
-            updateIpWhitelist(this.form).then(response => {
-              if (response.code === 200) {
-                this.$modal.msgSuccess("修改成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.$modal.msgError(response.message);
-              }
-            });
-          } else {
-            addIpWhitelist(this.form).then(response => {
-              if (response.code === 200) {
-                this.$modal.msgSuccess("新增成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.$modal.msgError(response.message);
-              }
-            });
-          }
-        }
-      });
+      this.ids = selection.map(item => item.id);
+      this.multiple = !selection.length;
     },
     /** 删除按钮操作 */
     handleDelete(row) {
       const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除IP白名单编号为"' + ids + '"的数据项？').then(function () {
-        return delIpWhitelist(ids);
+      this.$modal.confirm('是否确认删除用户登录日志编号为"' + ids + '"的数据项？').then(function () {
+        return delMemberLoginLog(ids);
       }).then(response => {
         if (response.code === 200) {
           this.getList();
@@ -283,19 +187,12 @@ export default {
         this.$modal.msgError(response.message);
       });
     },
-    /** 刷新缓存按钮操作 */
-    handleRefreshCache() {
-      this.$modal.confirm('是否确认刷新IP白名单缓存？').then(function () {
-        return refreshCache();
-      }).then(response => {
-        if (response.code === 200) {
-          this.$modal.msgSuccess("刷新缓存成功");
-        } else {
-          this.$modal.msgError(response.message);
-        }
-      }).catch(() => {
-        this.$modal.msgError(response.message);
-      });
+    /** 添加日期范围 */
+    addDateRange() {
+      if (this.dateRange != null && this.dateRange.length === 2) {
+        this.queryParams.beginTime = this.dateRange[0];
+        this.queryParams.endTime = this.dateRange[1];
+      }
     }
   }
 };
