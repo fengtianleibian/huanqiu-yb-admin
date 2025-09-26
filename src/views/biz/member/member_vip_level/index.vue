f66438ee5c2158cf59515e5de8388a1af694a6b7
Eason <>
2025-09-26 14:26:05 +0700

    no message

---
+++ src/views/biz/member/member_vip_level/index.vue
@@ -0,0 +1,302 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="IP地址" prop="ip">
+        <el-input
+          v-model="queryParams.ip"
+          placeholder="请输入IP地址"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['ip_whitelist:add']"
+        >新增
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['ip_whitelist:edit']"
+        >修改
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['ip_whitelist:remove']"
+        >删除
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-refresh"
+          size="mini"
+          @click="handleRefreshCache"
+          v-hasPermi="['ip_whitelist:remove']"
+        >刷新缓存
+        </el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="ipWhitelistList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="ID" align="center" prop="id"/>
+      <el-table-column label="IP地址" align="center" prop="ip"/>
+      <el-table-column label="创建时间" align="center" prop="createTime">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="更新时间" align="center" prop="updateTime">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.updateTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['ip_whitelist:edit']"
+          >修改
+          </el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['ip_whitelist:remove']"
+          >删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改IP白名单对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="IP地址" prop="ip">
+          <el-input v-model="form.ip" placeholder="请输入IP地址"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import {
+  listIpWhitelist,
+  getIpWhitelist,
+  delIpWhitelist,
+  addIpWhitelist,
+  updateIpWhitelist,
+  refreshCache
+} from "@/api/system/ip_whitelist";
+
+export default {
+  name: "IpWhitelist",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // IP白名单表格数据
+      ipWhitelistList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        ip: undefined
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        ip: [
+          {required: true, message: "IP地址不能为空", trigger: "blur"},
+          {pattern: /^(\d{1,3}\.){3}\d{1,3}$/, message: "请输入正确的IP地址格式", trigger: "blur"}
+        ]
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询IP白名单列表 */
+    getList() {
+      this.loading = true;
+      listIpWhitelist(this.queryParams).then(response => {
+        if (response.code === 200) {
+          this.ipWhitelistList = response.content.list;
+          this.total = response.content.total;
+          this.loading = false;
+        } else {
+          this.$modal.msgError(response.message);
+        }
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: undefined,
+        ip: undefined
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length != 1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加IP白名单";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getIpWhitelist(id).then(response => {
+        if (response.code === 200) {
+          this.form = response.content;
+          this.open = true;
+          this.title = "修改IP白名单";
+        } else {
+          this.$modal.msgError(response.message);
+        }
+      });
+    },
+    /** 提交按钮 */
+    submitForm: function () {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != undefined) {
+            updateIpWhitelist(this.form).then(response => {
+              if (response.code === 200) {
+                this.$modal.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.$modal.msgError(response.message);
+              }
+            });
+          } else {
+            addIpWhitelist(this.form).then(response => {
+              if (response.code === 200) {
+                this.$modal.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.$modal.msgError(response.message);
+              }
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除IP白名单编号为"' + ids + '"的数据项？').then(function () {
+        return delIpWhitelist(ids);
+      }).then(response => {
+        if (response.code === 200) {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        } else {
+          this.$modal.msgError(response.message);
+        }
+      }).catch(() => {
+        this.$modal.msgError(response.message);
+      });
+    },
+    /** 刷新缓存按钮操作 */
+    handleRefreshCache() {
+      this.$modal.confirm('是否确认刷新IP白名单缓存？').then(function () {
+        return refreshCache();
+      }).then(response => {
+        if (response.code === 200) {
+          this.$modal.msgSuccess("刷新缓存成功");
+        } else {
+          this.$modal.msgError(response.message);
+        }
+      }).catch(() => {
+        this.$modal.msgError(response.message);
+      });
+    }
+  }
+};
+</script>
