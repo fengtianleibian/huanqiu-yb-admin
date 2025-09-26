47c349434d86c1168df50b765219b821e1778c3c
Eason <>
2025-09-26 19:44:48 +0700

    银行列表

--- src/views/biz/pay/bank/index.vue
+++ src/views/biz/pay/bank/index.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="域名" prop="domain">
+      <el-form-item label="银行名称" prop="bankName">
         <el-input
-          v-model="queryParams.domain"
-          placeholder="请输入域名"
+          v-model="queryParams.bankName"
+          placeholder="请输入银行名称"
           clearable
           @keyup.enter.native="handleQuery"
         />
+      </el-form-item>
+      <el-form-item label="银行编号" prop="bankCode">
+        <el-input
+          v-model="queryParams.bankCode"
+          placeholder="请输入银行编号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="币种类型" prop="currencyType">
+        <el-input
+          v-model="queryParams.currencyType"
+          placeholder="请输入币种类型"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择状态" clearable>
+          <el-option label="正常" :value="0"></el-option>
+          <el-option label="停用" :value="1"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -23,7 +45,7 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['domain:add']"
+          v-hasPermi="['bank:add']"
         >新增
         </el-button>
       </el-col>
@@ -35,7 +57,7 @@
           size="mini"
           :disabled="single"
           @click="handleUpdate"
-          v-hasPermi="['domain:edit']"
+          v-hasPermi="['bank:update']"
         >修改
         </el-button>
       </el-col>
@@ -47,44 +69,28 @@
           size="mini"
           :disabled="multiple"
           @click="handleDelete"
-          v-hasPermi="['domain:remove']"
+          v-hasPermi="['bank:remove']"
         >删除
-        </el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-refresh"
-          size="mini"
-          @click="handleRefreshCache"
-          v-hasPermi="['domain:remove']"
-        >刷新缓存
         </el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="domainList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="bankList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center"/>
-      <el-table-column label="ID" align="center" prop="id"/>
-      <el-table-column label="域名" align="center" prop="domain"/>
-      <el-table-column label="默认代理" align="center" prop="defaultProxy"/>
-      <el-table-column label="默认代理ID" align="center" prop="defaultProxyId"/>
-      <el-table-column label="默认会员类型" align="center" prop="defaultMemberType">
+      <el-table-column label="银行名称" align="center" prop="bankName"/>
+      <el-table-column label="银行编号" align="center" prop="bankCode"/>
+      <el-table-column label="币种类型" align="center" prop="currencyType"/>
+      <el-table-column label="状态" align="center" prop="status">
         <template slot-scope="scope">
-          <span>{{ getMemberTypeText(scope.row.defaultMemberType) }}</span>
+          <el-tag :type="scope.row.status === 0 ? 'success' : 'danger'">
+            {{ scope.row.status === 0 ? '正常' : '停用' }}
+          </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="备注" align="center" prop="remark" show-overflow-tooltip/>
       <el-table-column label="创建时间" align="center" prop="createTime">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="更新时间" align="center" prop="updateTime">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.updateTime) }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
@@ -94,7 +100,7 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['domain:edit']"
+            v-hasPermi="['bank:update']"
           >修改
           </el-button>
           <el-button
@@ -102,7 +108,7 @@
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['domain:remove']"
+            v-hasPermi="['bank:remove']"
           >删除
           </el-button>
         </template>
@@ -117,23 +123,23 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改域名对话框 -->
+    <!-- 添加或修改银行对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="100px">
-        <el-form-item label="域名" prop="domain">
-          <el-input v-model="form.domain" placeholder="请输入域名"/>
+        <el-form-item label="银行名称" prop="bankName">
+          <el-input v-model="form.bankName" placeholder="请输入银行名称"/>
         </el-form-item>
-        <el-form-item label="默认代理" prop="defaultProxy">
-          <el-input v-model="form.defaultProxy" placeholder="请输入默认代理"/>
+        <el-form-item label="银行编号" prop="bankCode">
+          <el-input v-model="form.bankCode" placeholder="请输入银行编号"/>
         </el-form-item>
-        <el-form-item label="会员类型" prop="defaultMemberType">
-          <el-select v-model="form.defaultMemberType" placeholder="请选择默认会员类型" style="width: 100%">
-            <el-option label="会员账号" :value="0"></el-option>
-            <el-option label="代理账号" :value="1"></el-option>
-          </el-select>
+        <el-form-item label="币种类型" prop="currencyType">
+          <el-input v-model="form.currencyType" placeholder="请输入币种类型"/>
         </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入备注" :rows="3"/>
+        <el-form-item label="状态" prop="status">
+          <el-radio-group v-model="form.status">
+            <el-radio :label="0">正常</el-radio>
+            <el-radio :label="1">停用</el-radio>
+          </el-radio-group>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -146,16 +152,15 @@
 
 <script>
 import {
-  listDomain,
-  getDomain,
-  delDomain,
-  addDomain,
-  updateDomain,
-  refreshCache
-} from "@/api/biz/domain";
+  listBank,
+  getBank,
+  delBank,
+  addBank,
+  updateBank
+} from "@/api/biz/bank";
 
 export default {
-  name: "Domain",
+  name: "Bank",
   data() {
     return {
       // 遮罩层
@@ -170,8 +175,8 @@ export default {
       showSearch: true,
       // 总条数
       total: 0,
-      // 域名表格数据
-      domainList: [],
+      // 银行表格数据
+      bankList: [],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
@@ -180,26 +185,26 @@ export default {
       queryParams: {
         pageNum: 1,
         pageSize: 10,
-        domain: undefined
+        bankName: undefined,
+        bankCode: undefined,
+        currencyType: undefined,
+        status: undefined
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
-        domain: [
-          {required: true, message: "域名不能为空", trigger: "blur"},
+        bankName: [
+          {required: true, message: "银行名称不能为空", trigger: "blur"},
         ],
-        defaultProxy: [
-          {required: true, message: "默认代理不能为空", trigger: "blur"}
+        bankCode: [
+          {required: true, message: "银行编号不能为空", trigger: "blur"}
         ],
-        defaultProxyId: [
-          {required: true, message: "默认代理ID不能为空", trigger: "blur"}
+        currencyType: [
+          {required: true, message: "币种类型不能为空", trigger: "blur"}
         ],
-        defaultMemberType: [
-          {required: true, message: "默认会员类型不能为空", trigger: "change"}
-        ],
-        remark: [
-          {max: 500, message: "备注长度不能超过500个字符", trigger: "blur"}
+        status: [
+          {required: true, message: "状态不能为空", trigger: "change"}
         ]
       }
     };
@@ -208,12 +213,12 @@ export default {
     this.getList();
   },
   methods: {
-    /** 查询域名列表 */
+    /** 查询银行列表 */
     getList() {
       this.loading = true;
-      listDomain(this.queryParams).then(response => {
+      listBank(this.queryParams).then(response => {
         if (response.code === 200) {
-          this.domainList = response.content.list;
+          this.bankList = response.content.list;
           this.total = response.content.total;
           this.loading = false;
         } else {
@@ -230,11 +235,10 @@ export default {
     reset() {
       this.form = {
         id: undefined,
-        domain: undefined,
-        defaultProxy: undefined,
-        defaultProxyId: undefined,
-        defaultMemberType: undefined,
-        remark: undefined
+        bankName: undefined,
+        bankCode: undefined,
+        currencyType: undefined,
+        status: 0
       };
       this.resetForm("form");
     },
@@ -258,17 +262,17 @@ export default {
     handleAdd() {
       this.reset();
       this.open = true;
-      this.title = "添加域名";
+      this.title = "添加银行";
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
       const id = row.id || this.ids
-      getDomain(id).then(response => {
+      getBank(id).then(response => {
         if (response.code === 200) {
           this.form = response.content;
           this.open = true;
-          this.title = "修改域名";
+          this.title = "修改银行";
         } else {
           this.$modal.msgError(response.message);
         }
@@ -279,7 +283,7 @@ export default {
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.form.id != undefined) {
-            updateDomain(this.form).then(response => {
+            updateBank(this.form).then(response => {
               if (response.code === 200) {
                 this.$modal.msgSuccess("修改成功");
                 this.open = false;
@@ -289,7 +293,7 @@ export default {
               }
             });
           } else {
-            addDomain(this.form).then(response => {
+            addBank(this.form).then(response => {
               if (response.code === 200) {
                 this.$modal.msgSuccess("新增成功");
                 this.open = false;
@@ -305,8 +309,8 @@ export default {
     /** 删除按钮操作 */
     handleDelete(row) {
       const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除域名编号为"' + ids + '"的数据项？').then(function () {
-        return delDomain(ids);
+      this.$modal.confirm('是否确认删除银行编号为"' + ids + '"的数据项？').then(function () {
+        return delBank(ids);
       }).then(response => {
         if (response.code === 200) {
           this.getList();
@@ -317,28 +321,6 @@ export default {
       }).catch(() => {
         this.$modal.msgError(response.message);
       });
-    },
-    /** 刷新缓存按钮操作 */
-    handleRefreshCache() {
-      this.$modal.confirm('是否确认刷新域名缓存？').then(function () {
-        return refreshCache();
-      }).then(response => {
-        if (response.code === 200) {
-          this.$modal.msgSuccess("刷新缓存成功");
-        } else {
-          this.$modal.msgError(response.message);
-        }
-      }).catch(() => {
-        this.$modal.msgError(response.message);
-      });
-    },
-    /** 获取会员类型文本 */
-    getMemberTypeText(type) {
-      const typeMap = {
-        0: '会员账号',
-        1: '代理账号'
-      };
-      return typeMap[type] || '未知';
     }
   }
 };
